fix the entropy gradient, but still not work, seems not expolration well

agent.py
@@ -8,8 +8,8 @@ from torch.distributions.independent import Independent
 from torch.distributions.kl import kl_divergence
 from torch.nn import functional as F
 from tqdm import tqdm
-# from memory import ExperienceReplay
-from memory2 import ExperienceReplay
+from memory import ExperienceReplay
+# from memory2 import ExperienceReplay
 # from buffer import ExperienceReplay
 from models import bottle, Encoder, ObservationModel, RewardModel, TransitionModel, ValueModel, ActorModel, PCONTModel
 import cv2
@@ -250,7 +250,7 @@ class Dreamer(Agent):
     pcont = pcont.detach()
 
     if imag_ac_logps is not None:
-      imag_values[1:] -= self.args.temp * imag_ac_logps.detach()  # TODO: add entropy here , should keep its gradient
+      imag_values[1:] -= self.args.temp * imag_ac_logps  # TODO: add entropy here , should keep its gradient
 
     returns = cal_returns(imag_rewards[:-1], imag_values[:-1], imag_values[-1], pcont[:-1], lambda_=self.args.disclam)
 
@@ -429,12 +429,13 @@ class Dreamer(Agent):
     if (not deterministic) and (not self.args.with_logprob):
       action = Normal(action, self.args.expl_amount).rsample()
     # clip the angle
-    action[:, 0].clamp_(min=self.args.angle_min, max=self.args.angle_max)
-    # clip the throttle
-    if self.args.fix_speed:
-      action[:, 1] = self.args.throttle_base
-    else:
-      action[:, 1].clamp_(min=self.args.throttle_min, max=self.args.throttle_max)
+    if not self.args.with_logprob:
+      action[:, 0].clamp_(min=self.args.angle_min, max=self.args.angle_max)
+      # clip the throttle
+      if self.args.fix_speed:
+        action[:, 1] = self.args.throttle_base
+      else:
+        action[:, 1].clamp_(min=self.args.throttle_min, max=self.args.throttle_max)
     # return action.cup().numpy()
     return action  # this is a Tonsor.cuda
 

models.py
@@ -222,18 +222,21 @@ class ActorModel(nn.Module):
   def __init__(self, action_size, belief_size, state_size, hidden_size, mean_scale=5, min_std=1e-4, init_std=5, activation_function="elu",
                fix_speed=False, throttle_base=0.3):
     super().__init__()
+    self.fix_speed = fix_speed
+    self.throtlle_base = throttle_base
     self.act_fn = getattr(F, activation_function)
     self.fc1 = nn.Linear(belief_size + state_size, hidden_size)
     self.fc2 = nn.Linear(hidden_size, hidden_size)
     self.fc3 = nn.Linear(hidden_size, hidden_size)
     self.fc4 = nn.Linear(hidden_size, hidden_size)
-    self.fc5 = nn.Linear(hidden_size, 2 * (action_size))
+    if self.fix_speed:
+      self.fc5 = nn.Linear(hidden_size, 2*(action_size-1))  # only use steering
+    else:
+      self.fc5 = nn.Linear(hidden_size, 2 * (action_size))
     self.min_std = min_std
     self.init_std = init_std
     self.mean_scale = mean_scale
 
-    self.fix_speed = fix_speed
-    self.throtlle_base = throttle_base
   # def forward(self, belief, state, deterministic=False, with_logprob=False):
   #   hidden = self.act_fn(self.fc1(torch.cat([belief, state], dim=-1)))
   #   # print("first hidden", hidden[0][1], self.fc1.weight[1])
@@ -308,9 +311,11 @@ class ActorModel(nn.Module):
     dist = torch.distributions.Normal(mean, std)
     # TanhTransform = ComposeTransform([AffineTransform(0., 2.), SigmoidTransform(), AffineTransform(-1., 2.)])
     if self.fix_speed:
-      transform = [AffineTransform(0., 2.), SigmoidTransform(), AffineTransform(-1., 2.),  # TanhTransform
-                   AffineTransform(loc=torch.tensor([0.0, self.throtlle_base]).to("cuda"),
-                                  scale=torch.tensor([1.0, 0.0]).to("cuda"))]  # TODO: this is limited at donkeycar env
+      # transform = [AffineTransform(0., 2.), SigmoidTransform(), AffineTransform(-1., 2.),  # TanhTransform
+      #              AffineTransform(loc=torch.tensor([0.0, self.throtlle_base]).to("cuda"),
+      #                             scale=torch.tensor([1.0, 0.0]).to("cuda"))]  # TODO: this is limited at donkeycar env
+      transform = [AffineTransform(0., 2.), SigmoidTransform(), AffineTransform(-1., 2.),]  # TanhTransform
+
     else:
       transform = [AffineTransform(0., 2.), SigmoidTransform(), AffineTransform(-1., 2.),  # TanhTransform
                    AffineTransform(loc=torch.tensor([0.0, self.throtlle_base]).to("cuda"),
@@ -327,15 +332,11 @@ class ActorModel(nn.Module):
 
     # not use logprob now
     if with_logprob:
-      if self.fix_speed:
-        logp_pi = dist.log_prob(action)[:, 0]  # only consider the entropy of the
-      else:
-        logp_pi = dist.log_prob(action).sum(dim=1)
+      logp_pi = dist.log_prob(action).sum(dim=1)
     else:
       logp_pi = None
     # action dim: [batch, act_dim], log_pi dim:[batch]
-    return action, logp_pi  # dist ~ tanh(Normal(mean, std)); remember when sampling, using rsample() to adopt the reparameterization trick
-
+    return action if not self.fix_speed else torch.cat((action, 0.3*torch.ones_like(action, requires_grad=False)), dim=-1), logp_pi  # dist ~ tanh(Normal(mean, std)); remember when sampling, using rsample() to adopt the reparameterization trick
 
 
 class SampleDist: